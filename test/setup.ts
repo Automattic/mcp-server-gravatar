fb0d83f8eec14ff105da0cd3b27632aca6bf663c
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-05-12 17:22:03 -0500

    Add test/setup.ts

---
+++ test/setup.ts
@@ -0,0 +1,13 @@
+import { afterEach, beforeEach } from 'vitest';
+
+// Global test setup
+beforeEach(() => {
+  // Any global setup needed for tests can go here
+  // console.log('Test setup: Global setup complete');
+});
+
+// Global test teardown
+afterEach(() => {
+  // Any global teardown needed for tests can go here
+  // console.log('Test teardown: Global teardown complete');
+});
