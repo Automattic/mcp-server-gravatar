6eb7b108a998f0b8442493be85bd8225f5fe2103
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-05-16 15:06:44 -0500

    Update ApiAdapters to return interfaces

--- src/services/adapters/index.ts
+++ src/services/adapters/index.ts
@@ -13,13 +13,20 @@ import { ExperimentalApi } from '../../generated/gravatar-api/apis/ExperimentalA
 import { createApiConfiguration } from '../../common/utils.js';
 import { RestApiAdapter } from './rest-api-adapter.js';
 import { LegacyApiAdapter } from './legacy-api-adapter.js';
+import {
+  IProfileApiAdapter,
+  IExperimentalApiAdapter,
+  IGravatarImageApiAdapter,
+} from './interfaces.js';
 import fetch from 'node-fetch';
 
 /**
  * Create a REST API adapter with the default configuration
  * @returns A new RestApiAdapter instance
  */
-export async function createRestApiAdapter(): Promise<RestApiAdapter> {
+export async function createRestApiAdapter(): Promise<
+  IProfileApiAdapter & IExperimentalApiAdapter
+> {
   const config = await createApiConfiguration();
   return new RestApiAdapter(new ProfilesApi(config), new ExperimentalApi(config));
 }
@@ -28,6 +35,6 @@ export async function createRestApiAdapter(): Promise<RestApiAdapter> {
  * Create a Legacy API adapter with the default fetch implementation
  * @returns A new LegacyApiAdapter instance
  */
-export function createLegacyApiAdapter(): LegacyApiAdapter {
+export function createLegacyApiAdapter(): IGravatarImageApiAdapter {
   return new LegacyApiAdapter(fetch);
 }
