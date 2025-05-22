545bd49e0fa4f8218eca51acb91ddc73560d3a7f
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-05-22 13:03:16 -0500

    Revert to using `hash` instead of `avatarIdentifier`

--- src/apis/avatar-image-api.ts
+++ src/apis/avatar-image-api.ts
@@ -4,7 +4,7 @@ import type { DefaultAvatarOption, Rating } from '../common/types.js';
 import { apiConfig } from '../config/server-config.js';
 
 export interface GetAvatarByIdParams {
-  avatarIdentifier: string;
+  hash: string;
   size?: number;
   defaultOption?: DefaultAvatarOption;
   forceDefault?: boolean;
@@ -19,12 +19,12 @@ export class AvatarImageApi {
    * Get a Gravatar image by its identifier
    */
   async getAvatarById(params: GetAvatarByIdParams): Promise<Buffer> {
-    if (!validateHash(params.avatarIdentifier)) {
-      throw new GravatarValidationError('Invalid avatar identifier format');
+    if (!validateHash(params.hash)) {
+      throw new GravatarValidationError('Invalid hash format');
     }
 
     // Build avatar URL
-    let url = `${apiConfig.avatarBaseUrl}/${params.avatarIdentifier}`;
+    let url = `${apiConfig.avatarBaseUrl}/${params.hash}`;
     const queryParams = new URLSearchParams();
 
     if (params.size) {
